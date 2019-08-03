697c776bf4c24910832f5fae06ab261f28428785
Александр <alek295@mail.ru>
2019-08-03 17:06:44 +0500

    Переделал все

--- parser2/src/main/java/Test1.java
+++ parser2/src/main/java/Test1.java
@@ -1,27 +1,25 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.net.URL;
-import java.sql.DriverManager;
-import java.sql.*;
 import java.util.concurrent.TimeUnit;
 
 public class Test1 {
 
-    public static void main(String[] args) throws InterruptedException, SQLException {
+    public static void main(String[] args) throws InterruptedException{
         while (true) {
-        BD myBD = new BD();
-        String[] info = getCurrencyExchange("USD");
-        myBD.myInsert(info[0],info[1]);
-        myBD.mySelect();
-        TimeUnit.HOURS.sleep(12);
+            try{
+                String[] info = getCurrencyExchange("USD");
+                DB.myInsert(info[0],info[1]);
+                DB.mySelect();
+                TimeUnit.HOURS.sleep(12);
+            } catch (Exception e) {
+                e.printStackTrace();
+                TimeUnit.MINUTES.sleep(5);
+            }
         }
 
     }
@@ -58,58 +56,3 @@ public class Test1 {
     }
 }
 
-class BD{
-
-    public static Connection getConnect() throws SQLException {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        return DriverManager.getConnection("jdbc:sqlite:g://my.db");
-    }
-
-    static void myCreate(){
-        final String create = "CREATE TABLE if not exists CURRENCY_RATE(date TEXT PRIMARY KEY, value TEXT)";
-        try(  Connection co = getConnect();
-              Statement statement = co.createStatement();
-        ){
-            statement.execute(create);
-        }
-        catch (Exception e){
-            System.err.println(e.getMessage());
-        }
-
-    }
-    static void mySelect(){
-        final String select = "SELECT date, value FROM CURRENCY_RATE ORDER BY date";
-        try (   Connection co = getConnect();
-                Statement stmt = co.createStatement();
-                ResultSet rs = stmt.executeQuery(select);)
-        {
-            while(rs.next()){
-                String myDate = rs.getString("Date");
-                String myValue = rs.getString("Value");
-                System.err.println(myDate+"\t|\t"+myValue);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-    public static void myInsert(Object date, Object value) throws SQLException {
-        final String sql = "INSERT OR IGNORE INTO CURRENCY_RATE (date, value) VALUES (?, ?)";
-
-        try (
-                Connection c = getConnect();
-                PreparedStatement stmt = c.prepareStatement(sql)
-        ) {
-            stmt.setObject(1, date);
-            stmt.setObject(2, value);
-            stmt.execute();
-            //System.err.println("Complete: " + date + "\t" + value);
-        }
-    }
-
-}
