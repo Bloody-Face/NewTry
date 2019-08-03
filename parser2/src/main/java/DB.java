a4e47993e4816f644188899830b3221d7c26d1a2
Александр <alek295@mail.ru>
2019-08-03 22:29:12 +0500

    Исправленная версия 2.0

--- parser2/src/main/java/DB.java
+++ parser2/src/main/java/DB.java
@@ -14,27 +14,23 @@ public class DB {
 
     public static void myCreate() throws SQLException {
         final String create = "CREATE TABLE if not exists CURRENCY_RATE(date TEXT PRIMARY KEY, value TEXT)";
-        Connection co = getConnect();
-        Statement statement = co.createStatement();
-        statement.execute(create);
-        statement.close();
-        co.close();
+        try(Connection co = getConnect();
+        Statement statement = co.createStatement();) {
+            statement.execute(create);
+        }
     }
     public static void mySelect() throws SQLException {
         final String select = "SELECT date, value FROM CURRENCY_RATE ORDER BY date";
-        Connection co = getConnect();
+        try(Connection co = getConnect();
         Statement stmt = co.createStatement();
-        ResultSet rs = stmt.executeQuery(select);
+        ResultSet rs = stmt.executeQuery(select);) {
 
-        while(rs.next()){
-            String myDate = rs.getString("Date");
-            String myValue = rs.getString("Value");
-            System.err.println(myDate+"\t|\t"+myValue);
+            while (rs.next()) {
+                String myDate = rs.getString("Date");
+                String myValue = rs.getString("Value");
+                System.err.println(myDate + "\t|\t" + myValue);
+            }
         }
-        rs.close();
-        stmt.close();
-        co.close();
-
 
     }
     public static void myInsert(Object date, Object value) throws SQLException {
