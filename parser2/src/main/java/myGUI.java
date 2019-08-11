9c26523979aa46d0d779751f40b6b5731aa114fd
Александр <alek295@mail.ru>
2019-08-11 12:51:07 +0500

    Версия с GUI 2.0

--- parser2/src/main/java/myGUI.java
+++ parser2/src/main/java/myGUI.java
@@ -1,10 +1,7 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -15,26 +12,28 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class myGUI extends Application {
-    private TableView<Parser.Item> table = new TableView<Parser.Item>();
-    public static void main(String[] args) {
-        launch(args);
-    }
-    private final ObservableList<Parser.Item> myList = FXCollections.observableArrayList();
+    private TableView<Parser.Item> table = new TableView<>();
+
+    private final ObservableList<Parser.Item> items = FXCollections.observableArrayList();
+
     private XYChart.Series<String,Number> res = new XYChart.Series<>();
+
     private TableColumn firstNameCol = new TableColumn("Date");
+
     private TableColumn lastNameCol = new TableColumn("Value");
+
+    public static void main(String[] args) {   launch(args);   }
     @Override
     public void start(Stage stage) {
-        final VBox vbox = new VBox();
-        final HBox hbox = new HBox();
-        final VBox vboxMain = new VBox();
-        Scene scene = new Scene(new Group());
+        Scene scene = new Scene(new BorderPane());
+
         stage.setTitle("Table View Sample");
         stage.setWidth(800);
         stage.setHeight(510);
@@ -44,58 +43,59 @@ public class myGUI extends Application {
 
         lastNameCol.setMinWidth(125);
         firstNameCol.setMinWidth(125);
-        vbox.setPadding(new Insets(10, 0, 10, 10));
         table.getColumns().addAll(firstNameCol, lastNameCol);
 
-        //график
+        // График
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Date");
 
         NumberAxis yAxis = new NumberAxis();
         yAxis.setLabel("Value");
 
-        LineChart<String,Number > lineChart = new LineChart<String, Number>(xAxis,yAxis);
+        LineChart<String,Number > lineChart = new LineChart<>(xAxis,yAxis);
         res.setName("USD");
 
 
-        myConnect();
+        refresh();
         lineChart.getData().add(res);
 
 
         final Button myButton = new Button("Refresh");
-        myButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                myList.clear();
-                //res.getData().add(new XYChart.Data<String,String>("xX","xe"));
-                res.getData().clear();
-                myConnect();
-            }
-        });
-        vbox.getChildren().addAll(label, table);
-        hbox.getChildren().addAll(vbox, lineChart);
-        vboxMain.getChildren().addAll(hbox, myButton);
-        ((Group) scene.getRoot()).getChildren().addAll(vboxMain);
+
+        myButton.setOnAction(e -> refresh());
+
+        final VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10, 0, 10, 10));
+        vBox.getChildren().addAll(label, table);
+        final HBox hBox = new HBox();
+        hBox.getChildren().addAll(vBox, lineChart);
+        final VBox vBoxMain = new VBox();
+        vBoxMain.getChildren().addAll(hBox, myButton);
+
+        ((BorderPane) scene.getRoot()).setCenter(vBoxMain);
         stage.setScene(scene);
 
         stage.show();
     }
-    private void myConnect(){
-        //Таблица
+    private void refresh(){
+        items.clear();
+        res.getData().clear();
+        // Таблица
         try{
-            DB.mySelectForTable(myList);
+            DB.mySelectForTable(items);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        table.setItems(myList);
+        table.setItems(items);
         firstNameCol.setCellValueFactory(
                 new PropertyValueFactory<Parser.Item, String>("Date"));
         lastNameCol.setCellValueFactory(
                 new PropertyValueFactory<Parser.Item, String>("Value"));
 
-        //График
-        for(Parser.Item Dop : myList){
-            res.getData().add(new XYChart.Data<String,Number>(Dop.getDate(),Float.valueOf(Dop.getValue().replace(",","."))));
+        // График
+        for(Parser.Item item : items){
+            Float value = Float.valueOf(item.getValue().replace(",","."));
+            res.getData().add(new XYChart.Data<String,Number>(item.getDate(),value));
 
         }
     }
